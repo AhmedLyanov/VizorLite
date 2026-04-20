82c98efab5332a91e141752c3999b9df2aca3f16
AhmedLyanov <amoshal1997@gmail.com>
2026-04-20 21:25:38 +0300

    feature: new styles for option

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -1,11 +1,35 @@
 import ReactDOM from 'react-dom/client';
 import '../index.css';
 import App from './App';
+import { ConfigProvider, theme } from 'antd';
+
+
+const { darkAlgorithm } = theme;
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
+   <ConfigProvider
+      theme={{
+        algorithm: darkAlgorithm,
+        token: {
+          colorPrimary: '#177ddc',
+          colorBgBase: '#141414',
+          colorTextBase: '#e5e5e5',
+        },
+        components: {
+          Popover: {
+            colorBgElevated: '#1f1f1f',
+            colorText: '#e5e5e5',
+          },
+        },
+      }}
+    >
     <App />
+
+    </ConfigProvider>
 );
 
